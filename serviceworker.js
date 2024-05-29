3c809a561b7de904e27bcd4cef415d6205ddae12
Hugo Luis Ruscitti <hugo.ruscitti@mercadolibre.com>
2024-05-29 13:09:21 -0300

    Realizando prueba para crear cache.

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,36 @@
-self.addEventListener('fetch', function(event) {
-  event.respondWith(fetch(event.request));
+const cacheName = "retro-player-cache";
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+function isCacheable(request) {
+  const url = new URL(request.url);
+  return !url.pathname.endsWith(".json");
+}
+
+async function cacheFirstWithRefresh(request) {
+  const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  });
+
+  return (await caches.match(request)) || (await fetchResponsePromise);
+}
+
+console.log('web worker');
+console.log(self);
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+self.addEventListener("fetch", (event) => {
+  if (isCacheable(event.request)) {
+    event.respondWith(cacheFirstWithRefresh(event.request));
+  }
 });
